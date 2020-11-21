56db5e2d33d518ce541781a3b550713fb0c61343
distr <distritp@gmail.com>
2020-11-21 12:05:39 +0100

    T025

---
+++ EjerciciosUD3/src/T025/Principal.java
@@ -0,0 +1,22 @@
+package T025;
+
+public class Principal {
+	public static void main(String[] args) {
+		a();
+	}
+	
+	private static void a() {
+		b(9);
+	}
+	
+	private static void b(int x) {
+		x--;
+		System.out.println(c(x));
+	}
+	
+	private static int c(int x) {
+		x--;
+		return x;
+	}
+	
+}
