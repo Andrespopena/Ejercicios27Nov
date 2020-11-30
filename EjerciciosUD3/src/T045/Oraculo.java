e00036df4e11bebeded7b56b2a7b4fb47690948b
distr <distritp@gmail.com>
2020-11-30 12:29:31 +0100

    T045

---
+++ EjerciciosUD3/src/T045/Oraculo.java
@@ -0,0 +1,27 @@
+package T045;
+
+public class Oraculo {
+	
+	int a1;
+	static int e1;
+	
+	private static void estatico1() {
+
+	}
+
+	private static void estatico2() {
+
+	}
+
+	private void metodo1() {
+
+	}
+
+	private void metodo2() {
+
+	}
+	
+	// No porque son privados
+	//No se puede.
+	//No y Si.
+}
