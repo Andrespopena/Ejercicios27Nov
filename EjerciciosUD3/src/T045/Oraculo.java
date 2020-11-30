d7f6f784800f653117fc086f11304b31f16b0b5d
distr <distritp@gmail.com>
2020-11-30 12:33:42 +0100

    T045 segunda parte

--- EjerciciosUD3/src/T045/Oraculo.java
+++ EjerciciosUD3/src/T045/Oraculo.java
@@ -6,19 +6,22 @@ public class Oraculo {
 	static int e1;
 	
 	private static void estatico1() {
-
+		estatico2();
+		a1 = 0; //Error tiene que ser estatico
+		e1 = 1;
 	}
 
 	private static void estatico2() {
-
+		metodo1(); //Error, tiene que ser estatico
 	}
 
 	private void metodo1() {
-
+		metodo2();
 	}
 
 	private void metodo2() {
-
+		a1 = 0;
+		e1= 0;
 	}
 	
 	// No porque son privados
