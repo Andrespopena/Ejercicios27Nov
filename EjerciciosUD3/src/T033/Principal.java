7e08c58d6e2e4e732b52b349d9fba871615f4053
distr <distritp@gmail.com>
2020-11-30 10:57:46 +0100

    T033

---
+++ EjerciciosUD3/src/T033/Principal.java
@@ -0,0 +1,12 @@
+package T033;
+
+public class Principal {
+	public static void main(String[] args) {
+		System.out.println(metodo(3, 5));
+		
+	}
+	
+	public static double metodo(int a, int b) {
+		return Math.pow(a, b);
+	}
+}
