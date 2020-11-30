93b8693332772119e353991b1e76ee595ad639fe
distr <distritp@gmail.com>
2020-11-30 11:51:29 +0100

    T034

---
+++ EjerciciosUD3/src/T034/Util.java
@@ -0,0 +1,32 @@
+package T034;
+
+public final class Util {
+	public static boolean intervalo0a200(int x) {
+		if (x >=0 && x<=20)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean intervalo0a19(int x) {
+		if (x >=0 && x<20)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean intervalo1a20(int x) {
+		if (x >0 && x<=20)
+			return true;
+		else
+			return false;
+	}
+	
+	public static boolean intervalo1a19(int x) {
+		if (x >0 && x<20)
+			return true;
+		else
+			return false;
+	}
+	
+}
